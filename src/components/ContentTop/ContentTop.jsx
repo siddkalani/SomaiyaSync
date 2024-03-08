e5c733054749f88f381ab3b2e10c364e3d5feec2
siddkalani <siddharthkalani6@gmail.com>
2024-03-09 02:44:46 +0530

    searchbar

--- src/components/ContentTop/ContentTop.jsx
+++ src/components/ContentTop/ContentTop.jsx
@@ -4,9 +4,13 @@ import { useContext } from "react";
 import { SidebarContext } from "../../context/sidebarContext";
 import { FaRegNewspaper } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const ContentTop = () => {
     const { toggleSidebar } = useContext(SidebarContext);
+    const [isActive, setIsActive] = useState(false)
+
+
     return (
         <div className="main-content-top">
             <div className="content-top-left">
@@ -15,16 +19,21 @@ const ContentTop = () => {
                 </button>
                 <h3 className="content-top-title">Home</h3>
             </div>
-            <div className="content-top-btns">
-                <input type="search" className="SearchBar" placeholder='eg : siddharth' id="" />
-                <button type="button" className="search-btn content-top-btn">
+            <div className="content-top-btns right-btn">
+                <div>
+                {isActive && <div className="searchBar2"><input type="search" className="SearchBar shadow-drop-center" placeholder='Search for compatible user' id="" /></div>}</div>
+                <div> <button onClick={() => { setIsActive((prev) => !prev) }} type="button" className="search-btn content-top-btn">
                     <img src={iconsImgs.search} alt="" />
                 </button>
-                <NavLink to="/NewsFeed">
-                    <button type="button" className="notification-btn content-top-btn">
-                        <FaRegNewspaper />
-                    </button>
-                </NavLink>
+                </div>
+
+                <div>
+                    <NavLink to="/NewsFeed">
+                        <button type="button" className="notification-btn content-top-btn">
+                            <FaRegNewspaper />
+                        </button>
+                    </NavLink>
+                </div>
             </div>
         </div>
     )
