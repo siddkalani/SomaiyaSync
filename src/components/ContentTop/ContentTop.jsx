6c41e0759c9940d679e8fbe8608cbe63b02e66a6
siddkalani <siddharthkalani6@gmail.com>
2024-03-09 10:34:36 +0530

    seearch

--- src/components/ContentTop/ContentTop.jsx
+++ src/components/ContentTop/ContentTop.jsx
@@ -7,37 +7,36 @@ import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
 const ContentTop = () => {
-  const { toggleSidebar } = useContext(SidebarContext);
-  return (
-    <div className="main-content-top">
-      <div className="content-top-left">
-        <button
-          type="button"
-          className="sidebar-toggler"
-          onClick={() => toggleSidebar()}
-        >
-          <img src={iconsImgs.menu} alt="" />
-        </button>
-        <h3 className="content-top-title">Home</h3>
-      </div>
-      <div className="content-top-btns">
-        <input
-          type="search"
-          className="SearchBar"
-          placeholder="eg : siddharth"
-          id=""
-        />
-        <button type="button" className="search-btn content-top-btn">
-          <img src={iconsImgs.search} alt="" />
-        </button>
-        <NavLink to="/NewsFeed">
-          <button type="button" className="notification-btn content-top-btn">
-            <FaRegNewspaper />
-          </button>
-        </NavLink>
-      </div>
-    </div>
-  );
-};
+    const { toggleSidebar } = useContext(SidebarContext);
+    const [isActive, setIsActive] = useState(false)
 
-export default ContentTop;
+
+    return (
+        <div className="main-content-top">
+            <div className="content-top-left">
+                <button type="button" className="sidebar-toggler" onClick={() => toggleSidebar()}>
+                    <img src={iconsImgs.menu} alt="" />
+                </button>
+                <h3 className="content-top-title">Home</h3>
+            </div>
+            <div className="content-top-btns right-btn">
+                <div>
+                {isActive && <div className="searchBar2"><input type="search" className="SearchBar shadow-drop-center" placeholder='Search for compatible user' id="" /></div>}</div>
+                <div> <button onClick={() => { setIsActive((prev) => !prev) }} type="button" className="search-btn content-top-btn">
+                    <img src={iconsImgs.search} alt="" />
+                </button>
+                </div>
+
+                <div>
+                    <NavLink to="/NewsFeed">
+                        <button type="button" className="notification-btn content-top-btn">
+                            <FaRegNewspaper />
+                        </button>
+                    </NavLink>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ContentTop
