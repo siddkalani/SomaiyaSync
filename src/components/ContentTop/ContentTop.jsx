5a70bb4163a956c96823d6f46c715e9b103c10d0
shreyaa-03 <shedgeshreyy03@gmail.com>
2024-03-10 23:36:15 +0530

    c1

--- src/components/ContentTop/ContentTop.jsx
+++ src/components/ContentTop/ContentTop.jsx
@@ -7,36 +7,57 @@ import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
 const ContentTop = () => {
-    const { toggleSidebar } = useContext(SidebarContext);
-    const [isActive, setIsActive] = useState(false)
+  const { toggleSidebar } = useContext(SidebarContext);
+  const [isActive, setIsActive] = useState(false);
 
-
-    return (
-        <div className="main-content-top">
-            <div className="content-top-left">
-                <button type="button" className="sidebar-toggler" onClick={() => toggleSidebar()}>
-                    <img src={iconsImgs.menu} alt="" />
-                </button>
-                <h3 className="content-top-title">Home</h3>
+  return (
+    <div className="main-content-top">
+      <div className="content-top-left">
+        <button
+          type="button"
+          className="sidebar-toggler"
+          onClick={() => toggleSidebar()}
+        >
+          <img src={iconsImgs.menu} alt="" />
+        </button>
+        <h3 className="content-top-title">Home</h3>
+      </div>
+      <div className="content-top-btns right-btn">
+        <div>
+          {isActive && (
+            <div className="searchBar2">
+              <input
+                type="search"
+                className="SearchBar shadow-drop-center"
+                placeholder="Search for compatible user"
+                id=""
+              />
             </div>
-            <div className="content-top-btns right-btn">
-                <div>
-                {isActive && <div className="searchBar2"><input type="search" className="SearchBar shadow-drop-center" placeholder='Search for compatible user' id="" /></div>}</div>
-                <div> <button onClick={() => { setIsActive((prev) => !prev) }} type="button" className="search-btn content-top-btn">
-                    <img src={iconsImgs.search} alt="" />
-                </button>
-                </div>
-
-                <div>
-                    <NavLink to="/NewsFeed">
-                        <button type="button" className="notification-btn content-top-btn">
-                            <FaRegNewspaper />
-                        </button>
-                    </NavLink>
-                </div>
-            </div>
+          )}
         </div>
-    )
-}
+        <div>
+          {" "}
+          <button
+            onClick={() => {
+              setIsActive((prev) => !prev);
+            }}
+            type="button"
+            className="search-btn content-top-btn"
+          >
+            <img src={iconsImgs.search} alt="" />
+          </button>
+        </div>
 
-export default ContentTop
+        <div>
+          <NavLink to="/NewsFeed">
+            <button type="button" className="notification-btn content-top-btn">
+              <FaRegNewspaper />
+            </button>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContentTop;
