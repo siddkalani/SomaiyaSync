30163d124edf9cdcc79659e2a1a39445a9a54ef9
shreyaa-03 <shedgeshreyy03@gmail.com>
2024-03-26 01:16:53 +0530

    searchar1

--- src/components/ContentTop/ContentTop.jsx
+++ src/components/ContentTop/ContentTop.jsx
@@ -1,45 +1,75 @@
 import { iconsImgs } from "../../utils/images";
+// import SearchIcon from "@mui/icons-material/Search";
+// import CloseIcon from "@mui/icons-material/Close";
 import "./ContentTop.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { SidebarContext } from "../../context/sidebarContext";
 import { FaRegNewspaper } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import ReactSearchBox from "react-search-box";
 import { PiListFill } from "react-icons/pi";
-// import axios from "axios";
-// import Table from "../../../SearchBar/Table";
-// // import data from "../../../SearchBar/data";
+import axios from "axios";
 
 const ContentTop = () => {
   const { toggleSidebar } = useContext(SidebarContext);
-//   const [query, setQuery] = useState("");
-//   const [showTable, setShowTable] = useState(false); // State to track whether to show the table
-//   const [data, setData] = useState([]);
 
+  const [search, setSearch] = useState("");
+  const [searchData, setSearchData] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(-1);
 
-//   useEffect(() => {
-//     const fetchStudents = async () => {
-//       const res = await axios.get(`http://localhost:4200/?q=${query}`);
-//       setData(res.data);
-//     };
-//     if (query.length > 2) {
-//       fetchStudents();
-//     }
-//   }, [query]);
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  const handleClose = () => {
+    setSearch("");
+    setSearchData([]);
+    setSelectedItem(-1);
+  };
+  const handleKeyDown = (e) => {
+    if (selectedItem < searchData.length) {
+      if (e.key === "ArrowUp" && selectedItem > 0) {
+        setSelectedItem((prev) => prev - 1);
+      } else if (
+        e.key === "ArrowDown" &&
+        selectedItem > searchData.length - 1
+      ) {
+        setSelectedItem((prev) => prev + 1);
+      } else if (e.key === "Enter" && selectedItem >= 0) {
+        window.open(searchData[selectedItem].show.url);
+      }
+    } else {
+      setSelectedItem(-1);
+    }
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (search !== "") {
+          const response = await axios.get(
+            `http://localhost:4200/api/contacts/search?q=${search}`
+          );
+          setSearchData(response.data);
+        } else {
+          setSearchData([]);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-//   const handleSearchInputChange = (e) => {
-//     const inputValue = e.target.value;
-//     setQuery(inputValue);
-//     setShowTable(inputValue.length > 0); // Show the table if user starts typing
-//   };
-
+    fetchData();
+  }, [search]);
 
   return (
     <div className="main-content-top">
       <div className="content-top-left">
-        <button type="button" className="sidebar-toggler" onClick={() => toggleSidebar()}>
-          <PiListFill color="white"/>
+        <button
+          type="button"
+          className="sidebar-toggler"
+          onClick={() => toggleSidebar()}
+        >
+          <PiListFill color="white" />
         </button>
         <h3 className="content-top-title">Home</h3>
       </div>
@@ -48,13 +78,38 @@ const ContentTop = () => {
           <input
             type="search"
             placeholder="Search for comaptible user"
-            // onFocus={() => setShowTable(query.length > 0)}
-            // onChange={handleSearchInputChange}
-            // value={query}
+            onChange={handleChange}
+            value={search}
+            onKeyDown={handleKeyDown}
           />
-          <i className="fa fa-search fa-lg"></i>
+          {/* <div className="search_icon">
+            {search === " " ? (
+              <SearchIcon />
+            ) : (
+              <CloseIcon onClick={handleClose} />
+            )}
+          </div> */}
         </div>
+        <i onClick={handleClose} className="fa fa-search fa-lg"></i>
 
+        <div className="searchResult">
+          {searchData.slice(0, 5).map((data, index) => {
+            return (
+              <a
+                // href={data.show.url}
+                className={
+                  selectedItem === index
+                    ? "search_suggestion_active"
+                    : "search_suggestion"
+                }
+                key={index}
+              >
+                {/* {data.show && data.show.name ? data.show.name : "Unknown"} */}
+                {data.name}
+              </a>
+            );
+          })}
+        </div>
       </div>
       <div className="NewsFeed">
         <NavLink to="/NewsFeed">
@@ -64,8 +119,7 @@ const ContentTop = () => {
         </NavLink>
       </div>
     </div>
-  )
-
-  }
+  );
+};
 
 export default ContentTop;
