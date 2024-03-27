3dcb8f4bea5c7820b707a5684aa6f0bc5c36c466
siddkalani <siddharthkalani6@gmail.com>
2024-03-27 14:52:01 +0530

    shreya

--- src/practice/App.jsx
+++ src/practice/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { info } from './info'
-
+import "./practice.css"
 const App = () => {
+    const[activeId , setActiveId] = useState(null)
+    const handleClick=(id)=>{
+        setActiveId(id)
+    }
+    
     return (
-        <div>
+        <div className='practice'>
             {info.map((data) => {
                 return <nav>
                     <ul>
-                        <li>{data.title}</li>
+                        <li key={data.id} onClick={()=>{handleClick(data.id)}} className={`${data.id===activeId? "active": null}`} >{data.title}</li>
                     </ul>
                     </nav>
                     })}
