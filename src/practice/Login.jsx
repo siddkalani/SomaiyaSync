336694325b1d71da17a65bddcd1e6c6299bd391d
siddkalani <siddharthkalani6@gmail.com>
2024-03-31 16:02:06 +0530

    email validation

--- src/practice/Login.jsx
+++ src/practice/Login.jsx
@@ -4,27 +4,57 @@ import boy from "./boy.svg"
 import Register from './Register'
 
 const Login = () => {
+    
+    const [email , setEmail] = useState("")
+    const [error  , setError] = useState('')
 
+    const checkEmail = (e) =>{
+        setEmail(e.target.value)
+        
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        
+        const regex = /^[a-zA-Z0-9._]+@[a-z]+\.[a-z]{2,6}$/
+        if(regex.test(email)){
+            setError("valid email address")
+        }
+        else if(email === "") {
+            setError("Please enter email")
+        }
+        else if(!regex.test(email)){
+            setError("Please enter a valid email address")
+        } 
+         else{
+            setError("")
+        }
+    }
     return (
         <div className="login-container">
             <div className="login-main">
                 <div className="yellow"></div>
                 <div className="blue"></div>
                 <div className="login-img"><img src={boy} alt="" /></div>
+                <form action="#" onSubmit={handleSubmit}>
                 <div className="login-form">
                     <div className="form-header">
                         <div className="sign-in">
-                            <h1>Sign-in</h1>
+                            <h1><span className='form-header-color'>Sign in</span></h1>
                         </div>
                     </div>
+
                     <div className="form-inputs">
                         <div className="input-email">
-
+                            
                             <input
                                 type='search'
                                 placeholder='E-mail'
                                 className='inputbox-email'
+                                value={email}
+                                onChange={checkEmail}
                             />
+                            <p className='email-error'>{error}</p>
                             </div>
 
                         <div className="input-pass">
@@ -40,8 +70,9 @@ const Login = () => {
 
 
                     </div>
-                    <div className="login-btn"><button>sign in</button></div>
+                    <div className="login-btn"><button className='sign-in-btn' onClick={handleSubmit}>sign in</button></div>
                 </div>
+                </form>
                 <div className="login-content">
                    <div className="login-content-header"><h1>Sign in to <br />SomaiyaSync</h1>
                     </div>
