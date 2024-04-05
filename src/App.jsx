5fda2153239edb59057cc4701239ea9ae8267828
shreyaa-03 <shedgeshreyy03@gmail.com>
2024-04-05 23:07:34 +0530

    ssssss

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,56 @@
-import './App.css';
-import Sidebar from './layout/Sidebar/Sidebar';
-import Content from './layout/Content/Content';
-import ContentTop from './components/ContentTop/ContentTop';
-import ContentMain from './components/ContentMain/ContentMain';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Inbox from './pages/Inbox/Inbox';
-import Contest from './pages/Contest/Contest';
-import Leaderboard from './pages/Leaderboard/Leaderboard';
-import Account from './pages/Accounts/Account';
-import Settings from './pages/Settings/Settings';
-import Footer from './components/Footer/Footer';
-import NewsFeed from './pages/NewsFeed/NewsFeed';
-import EditProfile from './pages/BasicDetails/EditProfile';
-import { useLocation } from 'react-router-dom';
+import "./App.css";
+import Sidebar from "./layout/Sidebar/Sidebar";
+import Content from "./layout/Content/Content";
+import ContentTop from "./components/ContentTop/ContentTop";
+import ContentMain from "./components/ContentMain/ContentMain";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inbox from "./pages/Inbox/Inbox";
+import Contest from "./pages/Contest/Contest";
+import Leaderboard from "./pages/Leaderboard/Leaderboard";
+import Account from "./pages/Accounts/Account";
+import Settings from "./pages/Settings/Settings";
+import Footer from "./components/Footer/Footer";
+import NewsFeed from "./pages/NewsFeed/NewsFeed";
+import EditProfile from "./pages/BasicDetails/EditProfile";
+import Login from "./practice/Login";
+import Register from "./practice/Register";
+import { useLocation } from "react-router-dom";
+import EmailVerify from "./practice/Email";
 
 function App() {
-
   return (
-
-    <div className='whole-main'>
+    <div className="whole-main">
       <BrowserRouter>
-        <div className='app'>
+        <div className="app">
           <Sidebar />
-          <div className='main-content'>
-
-             <Routes>
+          <div className="main-content">
+            <Routes>
               {/* <Route index element={<ContentMain />}>
 
               </Route>  */}
-              <Route path='/home' element={<ContentMain />} >
+              <Route path="/register" element={<Register />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route
+                path="/api/users/verify/:id"
+                element={<EmailVerify />}
+              ></Route>
 
-              </Route>
-              <Route path='/contest' element={<Contest />} >
-
-              </Route>
-              <Route path='/inbox' element={<Inbox />} >
-
-              </Route>
-              <Route path='/account' element={<Account />} >
-
-              </Route>
-              <Route path='/leaderboard' element={<Leaderboard />} >
-
-              </Route>
-              <Route path='/settings' element={<Settings />} >
-
-              </Route>
-              <Route path='/NewsFeed' element={<NewsFeed />} >
-
-              </Route>
+              <Route path="/home" element={<ContentMain />}></Route>
+              <Route path="/contest" element={<Contest />}></Route>
+              <Route path="/inbox" element={<Inbox />}></Route>
+              <Route path="/account" element={<Account />}></Route>
+              <Route path="/leaderboard" element={<Leaderboard />}></Route>
+              <Route path="/settings" element={<Settings />}></Route>
+              <Route path="/NewsFeed" element={<NewsFeed />}></Route>
               {/* <Route path='/EditProfile' element={<EditProfile/>} >
 
               </Route> */}
-
             </Routes>
           </div>
         </div>
       </BrowserRouter>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import Login from './practice/Login.jsx';
-import Register from './practice/Register.jsx';
-import Quiz from './pages/Contest/games/quiz.jsx';
+import ReactDOM from "react-dom/client";
+import Login from "./practice/Login.jsx";
+import Register from "./practice/Register.jsx";
+import Quiz from "./pages/Contest/games/quiz.jsx";
+import App from "./App.jsx";
+import Account from "./pages/Accounts/Account.jsx";
 
-import { SidebarProvider } from './context/sidebarContext.jsx';
+import { SidebarProvider } from "./context/sidebarContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <SidebarProvider>
     <Login />
   </SidebarProvider>
-)
+);
