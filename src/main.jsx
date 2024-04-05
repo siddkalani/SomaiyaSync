cb9143ad7675f4485cbbdc5cb97cd40fa3ff6e2a
siddkalani <siddharthkalani6@gmail.com>
2024-04-05 23:09:23 +0530

    sidd

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client'
-import Login from './practice/Login.jsx';
 import Register from './practice/Register.jsx';
+import App from './App.jsx';
 import Quiz from './pages/Contest/games/quiz.jsx';
-
+import Login from './practice/Login.jsx';
 import { SidebarProvider } from './context/sidebarContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <SidebarProvider>
-    <Login />
+    <App/>
   </SidebarProvider>
 )
