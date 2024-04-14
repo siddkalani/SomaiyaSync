36c52eb8c9a1b5d98893248a94114d499250126d
shreya <shedgeshreyy03@gmail.com>
2024-04-14 20:31:57 +0530

    shreyaa2

--- somaiyasync/mycontacts/models/contactModel.js
+++ somaiyasync/mycontacts/models/contactModel.js
@@ -1,32 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const contactSchema = mongoose.Schema(
-//   {
-//     user_id: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       required: true,
-//       ref: "User",
-//     },
-//     name: {
-//       type: String,
-//       required: [true, "Please add the contact name"],
-//     },
-//     email: {
-//       type: String,
-//       required: [true, "Please add the email id "],
-//     },
-//     phone: {
-//       type: String,
-//       required: [true, "Please add the phone number "],
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
-
-// module.exports = mongoose.model("Contact", contactSchema);
-
 const mongoose = require("mongoose");
 
 const educationSchema = mongoose.Schema({
