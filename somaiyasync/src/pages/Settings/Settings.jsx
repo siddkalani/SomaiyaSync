923a0b9340ee905d733c72f69a812e54d3df0f6a
siddkalani <siddharthkalani6@gmail.com>
2024-04-14 12:16:31 +0530

    sidd

---
+++ somaiyasync/src/pages/Settings/Settings.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ContentTop from '../../components/ContentTop/ContentTop'
+import { useLocation } from 'react-router-dom'
+import Sidebar from '../../layout/Sidebar/Sidebar'
+  
+
+const Setting = () => {
+//   const locate = useLocation();
+//   const topName = locate.state.topName
+  return (
+    <div className="">
+    <div className='whole-main'>
+    <div className='app'>
+    <Sidebar />
+    <div className='main-content'>
+
+      <ContentTop/>
+
+      </div>
+      </div>
+      </div>
+      </div>
+  )
+}
+
+export default Setting
