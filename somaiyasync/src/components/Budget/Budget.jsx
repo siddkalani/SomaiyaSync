f0a75f933a71f5387f2b250e05c1d5b1eb10e095
siddkalani <siddharthkalani6@gmail.com>
2024-10-22 13:59:24 +0530

    sid

--- somaiyasync/src/components/Budget/Budget.jsx
+++ somaiyasync/src/components/Budget/Budget.jsx
@@ -15,25 +15,7 @@ const Budget = () => {
             <h2 className="lg-value">Types of Account</h2>
             {/* <span className="lg-value">$ 100,000</span> */}
         </div>
-        <div className="grid-c4-content bg-jet">
-            <div className="grid-items">
-                {
-                    budget.map((budget) => (
-                        <div className="grid-item" key = { budget.id }>
-                            <div className="grid-item-l">
-                                <div className="icon">
-                                    <img src={ iconsImgs.check } />
-                                </div>
-                                <p className="text text-silver-v1">{ budget.title } <span>{ budget.type }</span></p>
-                            </div>
-                            <div className="grid-item-r">
-                                <span className="text-silver-v1"> { budget.amount }</span>
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
-        </div>
+       
     </div>
   )
 }
