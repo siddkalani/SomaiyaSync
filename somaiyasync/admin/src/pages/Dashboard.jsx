67e15d41da6914608201e14a3765502f6f911001
siddkalani <siddharthkalani6@gmail.com>
2024-10-22 12:25:00 +0530

    input box

--- somaiyasync/admin/src/pages/Dashboard.jsx
+++ somaiyasync/admin/src/pages/Dashboard.jsx
@@ -85,8 +85,9 @@ const Dashboard = () => {
           <StatCard Icon={XOctagon} title="Submissions" value={983} change="4.5%" changeType="negative" />
         </div>
 
+      <div className='w-full flex space-x-8'>
 
-        <div className="grid grid-cols-1 lg:grid-cols-1 gap-6 mb-8">
+        <div className="w-2/3 grid grid-cols-1 lg:grid-cols-1 gap-6 mb-8">
 
           <div className="lg:col-span-2 bg-white p-6 rounded-lg shadow">
             <h2 className="text-xl font-bold mb-4">User Activity</h2>
@@ -106,64 +107,43 @@ const Dashboard = () => {
             <div className="w-1/3 flex flex-col h-full">
             <div className="bg-white rounded-lg shadow flex-grow flex flex-col h-full">
               <div className="p-4 border-b">
-                <h2 className="text-xl font-semibold">Add Product</h2>
+                <h2 className="text-xl font-semibold">Add News</h2>
               </div>
               <div className="p-4 flex-grow flex flex-col">
                 <form className="space-y-4 flex-grow flex flex-col">
-                  <div className=''>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">Image</label>
-                    <div className="border-2 border-dashed border-gray-300 rounded-md p-4 text-center">
-                      <Upload className="mx-auto text-gray-400" size={24} />
-                      <p className="mt-1 text-sm text-gray-500">Upload or drag image</p>
-                    </div>
-                  </div>
+                
                   <div className="flex space-x-4">
                     <div className="flex-1">
                       <label className="block text-sm font-medium text-gray-700 mb-1">Category</label>
                       <div className="relative">
-                        <select className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md">
-                          <option>Select</option>
-                          <option>Food</option>
-                          <option>Beverage</option>
-                          <option>Appetizer</option>
-                          <option>Dessert</option>
+                        <select className="block w-full pl-3 pr-10 py-3 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md">
+                          <option>Contest</option>
+                          <option>Hackathon</option>
+                          <option>Internship</option>
+                          <option>Placement</option>
                         </select>
                         {/* <ChevronDown className="absolute right-3 top-2.5 text-gray-400" size={18} /> */}
                       </div>
                     </div>
-                    <div className="flex-1">
-                      <label className="block text-sm font-medium text-gray-700 mb-1">Unit Price</label>
-                      <input
-                        type="text"
-                        className="block w-full pl-3 pr-3 sm:text-sm border-gray-300 rounded-md"
-                        placeholder="$0"
-                      />
-                    </div>
+                   
                   </div>
                   <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">Product Name</label>
+                    <label className="block text-sm font-medium text-gray-700 mb-1">News Title</label>
                     <input
                       type="text"
                       className="block w-full sm:text-sm border-gray-300 rounded-md"
-                      placeholder="Input product name"
+                      placeholder="News title"
                     />
                   </div>
                   <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">Product Code</label>
+                    <label className="block text-sm font-medium text-gray-700 mb-1">Description</label>
                     <input
                       type="text"
                       className="block w-full sm:text-sm border-gray-300 rounded-md"
-                      placeholder="####"
+                      placeholder="Here is my first news for somaiya"
                     />
                   </div>
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">Stock</label>
-                    <input
-                      type="number"
-                      className="block w-full sm:text-sm border-gray-300 rounded-md"
-                      placeholder="0"
-                    />
-                  </div>
+                
                   <div className="flex space-x-4 mt-auto">
                     <button
                       type="reset"
@@ -181,6 +161,7 @@ const Dashboard = () => {
                 </form>
               </div>
             </div>
+          </div>
           </div>
       </div>
     </div>
